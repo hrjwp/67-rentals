75d094788f5a1ec1b3d30a64bd7c53ddbabfb1c6
nicole <nicolemangg22@gmail.com>
2025-12-11 23:27:08 +0800

    implemented aes encryption

--- config.py
+++ config.py
@@ -6,6 +6,8 @@ class Config:
 
     # Flask
     SECRET_KEY = os.environ.get('SECRET_KEY', 'your-secret-key-here-change-in-production')
+    # AES data encryption key (URL-safe base64, 16/24/32 bytes when decoded)
+    DATA_ENCRYPTION_KEY = os.environ.get('DATA_ENCRYPTION_KEY')
 
     # Stripe
     STRIPE_API_KEY = 'sk_test_51SVUC6Cddk4teDLIfl8dquKj8ZAFQYyyuKQ7Oizda4SampGMLUENpCpIG3r33VnHV8zER3vqBZelZclbeCy76bGp00gXIme9XE'
