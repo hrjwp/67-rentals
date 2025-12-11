e702ecf9eb482515a8efd08a1959643c77a75854
nicole <nicolemangg22@gmail.com>
2025-12-11 23:46:35 +0800

    implement cookies

--- config.py
+++ config.py
@@ -8,6 +8,11 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'your-secret-key-here-change-in-production')
     # AES data encryption key (URL-safe base64, 16/24/32 bytes when decoded)
     DATA_ENCRYPTION_KEY = os.environ.get('DATA_ENCRYPTION_KEY')
+    # Secure cookies
+    SESSION_COOKIE_SECURE = True       # send cookies only over HTTPS
+    SESSION_COOKIE_HTTPONLY = True     # block JS access to cookies
+    SESSION_COOKIE_SAMESITE = 'Lax'    # CSRF-hardening for most flows
+    PREFERRED_URL_SCHEME = 'https'
 
     # Stripe
     STRIPE_API_KEY = 'sk_test_51SVUC6Cddk4teDLIfl8dquKj8ZAFQYyyuKQ7Oizda4SampGMLUENpCpIG3r33VnHV8zER3vqBZelZclbeCy76bGp00gXIme9XE'
